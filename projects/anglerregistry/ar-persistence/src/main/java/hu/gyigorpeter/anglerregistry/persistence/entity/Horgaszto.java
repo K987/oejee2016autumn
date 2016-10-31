6bd3dd14c6ce74591f2907438337e1f1cfdd28f4
petergyigor <peter.gyigor@gmail.com>
2016-10-31 15:15:44 +0100

    ADD create main entities

---
+++ projects/anglerregistry/ar-persistence/src/main/java/hu/gyigorpeter/anglerregistry/persistence/entity/Horgaszto.java
@@ -0,0 +1,68 @@
+package hu.gyigorpeter.anglerregistry.persistence.entity;
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "horgaszto")
+public class Horgaszto implements Serializable {
+
+	@Transient
+	private static final long serialVersionUID = 49619221020832690L;
+	
+	@Id
+	@Column(name="id", nullable=false)
+	private long id;
+	
+	@Column(name="vizterkod", nullable=false)
+	private String vizterKod;
+	
+	@Column(name="terulet", nullable=false)
+	private int terulet;
+	
+	@Column(name="to_tipusa")
+	private String toTipus;
+	
+	@Column(name="legnagyobbvizmelyseg")
+	private int legnagyobbVizmelyseg;
+	
+	@Column(name="tulajdonos", nullable=false)
+	private String tulajdonos;
+	
+	@Column(name="horgasztanya", nullable=false)
+	private boolean horgasztanya;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
